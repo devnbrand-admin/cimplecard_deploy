163a524750f3284153dc60ab9785e028eecb75a8
prahul25 <palrahulkumar2002@gmail.com>
2024-12-12 00:13:48 +0530

    Completion of b2b-template and Completed one component of astrologer template

--- client/app/cards/astrologer/[id]/page.jsx
+++ client/app/cards/astrologer/[id]/page.jsx
@@ -1,17 +1,6 @@
 'use client'
 import React from "react";
-import GuidanceSection from "../../../components/astrocomponents/GuidanceSection";
-import Header from "../../../components/astrocomponents/Header";
-import HeroSection from "../../../components/astrocomponents/HeroSection";
-import PsychicReadings from "../../../components/astrocomponents/PsychicReadings";
-import StayUpdated from "../../../components/astrocomponents/StayUpdated";
-import Testimonials from "../../../components/astrocomponents/Testimonials";
-// import "@/app/style/astrologer.css";
-import SocialMediaEmbeds from "../../../components/astrocomponents/SocialMediaHandlers";
-import ContactInfo from "../../../components/astrocomponents/ContactUs";
-import WhatsAppButton from "../../../components/astrocomponents/WhatsappContact";
-import ContactCardButton from "../../../components/astrocomponents/ContactCardButton";
-import Footer from "../../../components/AstroComponents/Footer";
+
 import Bg from "../../../assets/astrologerTemplate/BgImage.png"
 import ProfileCard from "../../../components/astrocomponents/Header";
 
@@ -30,31 +19,7 @@ const App = () => {
       {/* Header */}
       <ProfileCard/>
      
-      {/* Hero Section */}
-      {/* <HeroSection/> */}
-
-      {/* guidance Section */}
-      {/* <GuidanceSection/> */}
       
-      {/* Reading Section */}
-      {/* <PsychicReadings/> */}
-
-      {/* SocialMediaEmbeds Section */}
-      {/* <SocialMediaEmbeds/> */}
-
-      {/* Testimonial Carousel Section */}
-      {/* <Testimonials/> */}
-
-      {/* <ContactInfo/> */}
-      {/* <ContactCardButton/> */}
-
-      {/* <WhatsAppButton/> */}
-      
-      {/* Subscribe Section */}
-      {/* <StayUpdated/> */}
-      
-      {/* Footer */}
-      {/* <Footer/> */}
     </div>
   );
 };
