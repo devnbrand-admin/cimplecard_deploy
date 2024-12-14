052547b614fbd8ab664f6c70ed8e25c7bb58242c
prahul25 <palrahulkumar2002@gmail.com>
2024-12-14 16:53:34 +0530

    Completed both b2b and astrologer template with added functionality

--- client/app/cards/b2b/[id]/page.jsx
+++ client/app/cards/b2b/[id]/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from "react";
+import React from "react";
 import AboutUs from "../../../components/b2bcomponents/AboutUs";
 import ContactForm from "../../../components/b2bcomponents/Appointment";
 import ContactInfo from "../../../components/b2bcomponents/ContactInfo";
