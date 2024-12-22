bbff4264f272b3fe4fcd5cc799dd125c99a1359c
Zer-0ne <sahilkhan8294799@gmail.com>
2024-12-22 15:19:52 +0530

    Improve styling and responsiveness for Home and About pages
    - Enhanced the overall styling for both Home and About pages.
    - Made layout adjustments to ensure better responsiveness across different screen sizes.
    - Updated component styles to improve user experience on mobile devices.

--- client/app/components/Home/homeComponent.jsx
+++ client/app/components/Home/homeComponent.jsx
@@ -9,7 +9,7 @@ import peopleImg from "../../assets/home/peopleImg.png";
 import Navbar from "../navbar/Navbar";
 import BrowseCard from "../Home/browseCard";
 import HowItOperates from "./howItOperates";
-import { handleJoinSignLoignfn } from "../AboutUS/aboutusComponent";
+import { HandleJoinSignLoignfn } from "../AboutUS/aboutusComponent";
 const HomeComponent = () => {
   const windowWidth = useWindowWidth();
   const [clipPath, setClipPath] = useState(
@@ -30,10 +30,10 @@ const HomeComponent = () => {
     return () => window.removeEventListener("resize", updateClipPath);
   }, []);
 
-  const handlesemicircleDataDis = () => {
+  const HandlesemicircleDataDis = () => {
     return (
       <>
-        <span className="mt-48 sm:mt-0 text-sm md:text-lg text-center w-4/5 md:w-2/3 font-medium text-white px-4 md:px-6 mb-4 md:mb-6">
+        <span className="mt-48 text-sm md:text-lg text-center w-4/5 md:w-2/3 font-medium text-white px-4 md:px-6 mb-4 md:mb-6">
           <span className="text-lg md:text-2xl font-bold">Cimply</span> Manage
           All Your Business Cards with Ease
           <br />
@@ -58,9 +58,20 @@ const HomeComponent = () => {
     <>
       <div className="flex flex-col h-100vh ">
         <div className="relative  text-white h-screen  flex justify-center items-center">
+
+          {/* Logo */}
+          <div
+            className=" self-start md:hidden z-20 flex justify-center items-center  mt-5"
+            style={{
+              borderRadius: "50%",
+            }}
+          >
+            <Image src={logoicon} alt="Logo" width={200} height={200} />
+          </div>
+
           {/* Semi-Circle Container */}
           <div
-            className="w-[100%] h-[350px] bottom-[150px] md:w-[1000px] md:h-[600px] md:bottom-[30px] sm:w-[550px] flex flex-col justify-start items-center absolute"
+            className="w-[100%] h-[350px] bottom-[100px] md:w-[1000px] md:h-[600px] md:bottom-[50px] sm:w-[550px] flex flex-col justify-start items-center absolute"
             style={{
               backgroundImage: `url(${people.src})`,
               backgroundRepeat: "no-repeat",
@@ -70,45 +81,37 @@ const HomeComponent = () => {
               borderRadius: "50% 50% 0 0",
             }}
           >
-            {/* Logo */}
-            <div
-              className="-mt-[150px] sm:-mt-[39px] z-20   flex justify-center items-center  mb-3"
-              style={{
-                borderRadius: "50%",
-              }}
-            >
-              <Image src={logoicon} alt="Logo" width={200} height={200} />
-            </div>
 
             {/* Text Content */}
 
-            {windowWidth > 600 && handlesemicircleDataDis()}
+            {windowWidth > 600 && <HandlesemicircleDataDis />}
           </div>
-        </div>
 
-        {/* Polygonal Divider Section */}
-        <div
-          className="absolute -bottom-10 w-[100%] flex-grow bg-[#3342a2] text-white flex flex-col justify-center items-center overflow-hidden"
-          style={{
-            clipPath: clipPath,
-          }}
-        >
-          <div className="relative w-full h-[400px]">
-            {windowWidth > 600 ? (
-              <Image
-                src={peopleImg}
-                alt="People"
-                fill
-                className="object-cover"
-                style={{
-                  objectPosition: "center",
-                }}
-              />
-            ) : (
-              <div></div>
-            )}
+
+          {/* Polygonal Divider Section */}
+          <div
+            className="absolute bottom-[20px] md:bottom-[40px] w-[100%] flex-grow bg-[#3342a2] text-white flex flex-col justify-center items-center overflow-hidden"
+            style={{
+              clipPath: clipPath,
+            }}
+          >
+            <div className={`relative w-full h-[400px]`}>
+              {windowWidth > 600 && (
+                <Image
+                  src={peopleImg}
+                  alt="People"
+                  fill
+                  className={`object-cover`}
+                  style={{
+                    objectPosition: "center",
+                  }}
+                />
+              )}
+            </div>
           </div>
-          <div className="absolute bottom-16 flex justify-center px-6">
+
+
+          <div className="absolute md:bottom-[6rem] bottom-[5rem] flex justify-center px-6">
             {windowWidth > 600 ? (
               <p className="text-sm hidden md:block md:text-base text-wrap mt-2 text-center leading-relaxed sm:w-full md:w-1/2">
                 Say goodbye to paper business cards and hello to a more
@@ -118,15 +121,15 @@ const HomeComponent = () => {
               </p>
             ) : (
               <div className=" flex flex-col text-center justify-start items-center -mb-[55px]">
-                {handlesemicircleDataDis()}
+                {<HandlesemicircleDataDis />}
               </div>
             )}
           </div>
         </div>
       </div>
       <HowItOperates />
-      <div className="w-2/3 md:-mt-[100px] md:w-1/2 mx-auto mb-10">
-        {handleJoinSignLoignfn()}
+      <div className="w-2/3 lg:-mt-[100px] md:w-1/2 mx-auto mb-10">
+        <HandleJoinSignLoignfn />
       </div>
       {/* <BrowseCard /> */}
     </>
--- client/app/components/Home/howItOperates.jsx
+++ client/app/components/Home/howItOperates.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import phoneMockup from '../../assets/home/cardImg/iphone.png'; 
+import phoneMockup from '../../assets/home/cardImg/iphone.png';
 import callIcon from "../../assets/home/howit/callIcon.svg"
 import whatsappIcon from "../../assets/home/howit/whatsappIcon.png"
 import mailIcon from "../../assets/home/howit/mailIcon.png"
@@ -10,23 +10,19 @@ import socailIcon from "../../assets/home/howit/socailIcon.png"
 import supportIcon from "../../assets/home/howit/supportIcon.png"
 
 
-
-
-
-
 const HowItOperates = () => {
   const features = [
-    { title: 'CLICK TO CALL', description: 'Your customers will reach out to you by just clicking on your phone number.',icon:callIcon },
-    { title: 'CLICK TO WHATSAPP', description: 'Your customers will reach out to you by just clicking on your phone number.' , icon:whatsappIcon},
-    { title: 'CLICK TO EMAIL', description: 'Your customers will reach out to you by just clicking on your phone number.',icon:mailIcon  },
-    { title: 'WEBSITE LINK', description: 'Your customers will reach out to you by just clicking on your phone number.', icon:broswerIcon },
+    { title: 'CLICK TO CALL', description: 'Your customers will reach out to you by just clicking on your phone number.', icon: callIcon },
+    { title: 'CLICK TO WHATSAPP', description: 'Your customers will reach out to you by just clicking on your phone number.', icon: whatsappIcon },
+    { title: 'CLICK TO EMAIL', description: 'Your customers will reach out to you by just clicking on your phone number.', icon: mailIcon },
+    { title: 'WEBSITE LINK', description: 'Your customers will reach out to you by just clicking on your phone number.', icon: broswerIcon },
   ];
 
   const featuresRight = [
-    { title: 'CLICK TO MAP', description: 'Your customers will reach out to you by just clicking on your phone number.',icon:callIcon },
-    { title: 'BANK DETAILS', description: 'Your customers will reach out to you by just clicking on your phone number.',icon: bankIcon },
-    { title: 'SOCIAL LINKS', description: 'Your customers will reach out to you by just clicking on your phone number.',icon: socailIcon },
-    { title: '24/7 SUPPORT', description: 'Your customers will reach out to you by just clicking on your phone number.',icon: supportIcon },
+    { title: 'CLICK TO MAP', description: 'Your customers will reach out to you by just clicking on your phone number.', icon: callIcon },
+    { title: 'BANK DETAILS', description: 'Your customers will reach out to you by just clicking on your phone number.', icon: bankIcon },
+    { title: 'SOCIAL LINKS', description: 'Your customers will reach out to you by just clicking on your phone number.', icon: socailIcon },
+    { title: '24/7 SUPPORT', description: 'Your customers will reach out to you by just clicking on your phone number.', icon: supportIcon },
   ];
 
   return (
@@ -38,62 +34,64 @@ const HowItOperates = () => {
 
       {/* Main Content */}
       <div className="flex flex-col lg:flex-row justify-center items-center gap-8">
-  {/* Features Left */}
-  <div className="space-y-8 px-14 md:px-0 -mt-[120px] text-center lg:text-right w-full lg:w-1/4 order-2 lg:order-1">
-    {features.map((feature, index) => (
-      <div key={index} className="flex flex-col lg:flex-row items-center gap-4">
-        <div className="text-[#161973]">
-          <h4 className="text-lg font-bold">{feature.title}</h4>
-          <p className="font-base text-sm text-center mt-2 text-[#161973]">{feature.description}</p>
+        {/* Features Left */}
+        <div className="space-y-8 px-14 md:px-0 self-start -mt-[150px] lg:-mt-0 text-center lg:text-right w-full lg:w-1/4 order-2 lg:order-1">
+          {features.map((feature, index) => (
+            <div key={index} className="flex flex-col lg:flex-row items-center gap-4">
+              <div className="text-[#161973]">
+                <h4 className="text-lg font-bold">{feature.title}</h4>
+                <p className="font-base text-sm text-center mt-2 text-[#161973]">{feature.description}</p>
+              </div>
+
+
+              <div className="hidden  w-[60px] h-[55px] border-[3px] border-gray-300 bg-[#d5d6e1] rounded-full lg:flex items-center justify-center shadow-md">
+                <div className="w-[50px] h-[50px] bg-[#acb3e2] rounded-full flex items-center justify-center">
+                  <Image
+                    src={feature?.icon}
+                    alt="Phone Icon"
+                    className="w-[24px] h-[24px] object-contain"
+                    priority
+                  />
+                </div>
+              </div>
+
+            </div>
+          ))}
         </div>
-        <div className="hidden w-[60px] h-[55px] border-[3px] border-gray-300 bg-[#d5d6e1] rounded-full md:flex items-center justify-center shadow-md">
-  <div className="w-[50px] h-[50px] bg-[#acb3e2] rounded-full flex items-center justify-center">
-    <Image
-      src={feature?.icon}
-      alt="Phone Icon"
-      className="w-[24px] h-[24px] object-contain"
-      priority
-    />
-  </div>
-</div>
 
-      </div>
-    ))}
-  </div>
+        {/* Center Image */}
+        <div className="w-full lg:w-1/3 h-[600px] order-1 lg:order-2">
+          <Image
+            src={phoneMockup}
+            alt="Phone Mockup"
 
-  {/* Center Image */}
-  <div className="w-full lg:w-1/3 h-[600px] order-1 lg:order-2">
-    <Image
-      src={phoneMockup}
-      alt="Phone Mockup"
-      
-      className="w-full h-full object-contain"
-      priority
-    />
-  </div>
+            className="w-full h-full object-contain"
+            priority
+          />
+        </div>
 
-  {/* Features Right */}
-<div className="space-y-8 px-14 md:px-0 -mt-[120px] text-center lg:text-left w-full lg:w-1/4 order-3">
-    {featuresRight.map((feature, index) => (
-      <div key={index} className="flex flex-col lg:flex-row items-center gap-4">
-        <div className="hidden w-[60px] h-[55px] border-[3px] border-gray-300 bg-[#d5d6e1] rounded-full md:flex items-center justify-center shadow-md">
-  <div className="w-[50px] h-[50px] bg-[#acb3e2] rounded-full flex items-center justify-center">
-    <Image
-      src={feature?.icon}
-      alt="Phone Icon"
-      className="w-[24px] h-[24px] object-contain"
-      priority
-    />
-  </div>
-</div>
-<div className="text-[#161973]">
-          <h4 className="text-lg font-bold">{feature.title}</h4>
-          <p className="font-base text-sm  mt-2 text-center text-[#161973]">{feature.description}</p>
+        {/* Features Right */}
+        <div className="space-y-8 px-14 md:px-0 self-start text-center lg:text-left w-full lg:w-1/4 order-3">
+          {featuresRight.map((feature, index) => (
+            <div key={index} className="flex flex-col lg:flex-row items-center gap-4">
+              <div className="hidden w-[60px] h-[55px] border-[3px] border-gray-300 bg-[#d5d6e1] rounded-full lg:flex items-center justify-center shadow-md">
+                <div className="w-[50px] h-[50px] bg-[#acb3e2] rounded-full flex items-center justify-center">
+                  <Image
+                    src={feature?.icon}
+                    alt="Phone Icon"
+                    className="w-[24px] h-[24px] object-contain"
+                    priority
+                  />
+                </div>
+              </div>
+              <div className="text-[#161973]">
+                <h4 className="text-lg font-bold">{feature.title}</h4>
+                <p className="font-base text-sm  mt-2 text-center text-[#161973]">{feature.description}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-    ))}
-  </div>
-</div>
 
     </div>
   );
